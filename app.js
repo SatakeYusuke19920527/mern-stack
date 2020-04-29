f42f8c1239ecd9006098371b11828356e7ea3fab
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-04-29 16:36:46 +0900

    server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var users2Router = require('./routes/users2');
 
 var app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users2', users2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
