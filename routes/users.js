d0c5c1d9eaf1d2b78e24bb34bd9d53eaabbeefd3
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-04-29 14:34:02 +0900

    mern-stack

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send([
+    { id: 1, name: 'satake' },
+    { id: 2, name: 'Yusuke' },
+    { id: 3, name: 'sat' },
+  ]);
+});
+
+module.exports = router;
